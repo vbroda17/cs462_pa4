81966a2e0fe279ed91580a95a830ff1098e91a08
Vincent Broda <101155730+vbroda17@users.noreply.github.com>
2023-11-01 01:21:23 -0400

    Hello world test running

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cs462_pa4
 my ocean but in mpi, for cs462 class
+
+to run
+su - base
+mpirun -np 4 ./myocean-mpi
--- myocean-mpi.c
+++ myocean-mpi.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
-#include <math.h>
+#include <mpi.h>
 
 
-int main(int argc, int **argv)
+int main(int argc, char *argv[])
 {
-  
+  int rank;
+  int size;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  printf("Hello World! From %d of %d\n", rank, size);
+  MPI_Finalize();
+  return 0;
 }
